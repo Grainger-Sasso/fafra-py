41610c1a7d1b8fc9546f2acd1385d351fcf4513e
Grainger-Sasso <gsasso16@gmail.com>
2021-03-28 11:50:02 -0400

    Added in new class structure, updated requirements.txt

---
+++ src/risk_classification/risk_classifiers/svm_risk_classifier/svm_risk_classifier.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cvxopt
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from sklearn.svm import LinearSVC
+from sklearn.metrics import confusion_matrix
+
+class SVMRiskClassifier:
+    def __init__(self):
+        pass
+
