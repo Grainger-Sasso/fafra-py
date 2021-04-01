6257027dfe1cd61c77f7bd345e27fb19adc77db6
Grainger-Sasso <gsasso16@gmail.com>
2021-04-01 16:35:30 -0400

    Updating the svm class and visualizer

--- src/risk_classification/risk_classifiers/svm_risk_classifier/svm_risk_classifier.py
+++ src/risk_classification/risk_classifiers/svm_risk_classifier/svm_risk_classifier.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cvxopt
-from sklearn.datasets.samples_generator import make_blobs
+from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 from sklearn.svm import SVC
@@ -9,9 +9,29 @@ from sklearn.metrics import confusion_matrix
 
 class SVMRiskClassifier:
     def __init__(self):
-        pass
+        self.model = None
 
-    def classify(self, x: np.array, y: np.array):
-        model = SVC(kernel='linear', C=1E10)
-        return model.fit(x, y)
+    def get_model(self):
+        return self.model
 
+    def set_model(self, model: SVC):
+        self.model = model
+
+    def generate_model(self, kernel='linear', C=1E10):
+        # TODO: come back and fix the input params for model
+        self.set_model(SVC(kernel=kernel, C=C))
+
+    def fit_model(self, x: np.ndarray, y: np.ndarray):
+        """
+        Fits SVM model to input training vectors, x, and target values, y (notation is canonically used)
+        :param x: Training vectors
+        :type: np.ndarray
+        :param y: Target values
+        :type: np.ndarray
+        :return: Trained model
+        :rtype: SVC
+        """
+        self.model.fit(x, y)
+
+    def make_prediction(self, samples):
+        return self.model.predict(samples)
