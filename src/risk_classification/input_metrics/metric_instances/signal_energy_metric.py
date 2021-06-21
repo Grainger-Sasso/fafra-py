201a9197245041e69e4e20c0834e6d982ddefa5b
Lingxiao Mou <13983860470mlx@gmail.com>
2021-06-22 07:46:17 +0800

    finished signal_metric editing

---
+++ src/risk_classification/input_metrics/metric_instances/signal_energy_metric.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from src.risk_classification.input_metrics.metric_names import MetricNames
+from src.risk_classification.input_metrics.risk_classification_input_metric import RiskClassificationInputMetric
+
+
+METRIC_NAME = MetricNames.SIGNAL_ENERGY
+
+
+class Metric(RiskClassificationInputMetric):
+    def __init__(self):
+        super(RiskClassificationInputMetric, self).__init__(METRIC_NAME)
+
+    def generate_metric(self, **kwargs):
+        data_array=np.array(kwargs['data'])# convert the data into numpy array
+                                            #take absolute value and square each element
+        data_array=np.abs(data_array)
+        data_array=np.square(data_array)
+        return np.sum(data_array)           #return the sum of the squared result to find the signal energy metric
+
+
+
--- src/risk_classification/input_metrics/metric_names.py
+++ src/risk_classification/input_metrics/metric_names.py
@@ -8,6 +8,7 @@ class MetricNames(Enum):
     MEAN = 'mean'
     ROOT_MEAN_SQUARE = 'rms'
     STANDARD_DEVIATION = 'std'
+    SIGNAL_ENERGY='se'
 
     def get_name(self):
         # Self is the member here
