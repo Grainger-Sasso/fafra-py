9938a8acf79a8436523df092559adf784d70a427
Lingxiao Mou <13983860470mlx@gmail.com>
2021-06-28 11:31:15 +0800

    push task20

--- src/risk_classification/input_metrics/metric_instances/signal_energy_metric.py
+++ src/risk_classification/input_metrics/metric_instances/signal_energy_metric.py
@@ -12,11 +12,11 @@ class Metric(RiskClassificationInputMetric):
         super(RiskClassificationInputMetric, self).__init__(METRIC_NAME)
 
     def generate_metric(self, **kwargs):
-        data_array=np.array(kwargs['data'])# convert the data into numpy array
-                                            #take absolute value and square each element
-        data_array=np.abs(data_array)
-        data_array=np.square(data_array)
-        return np.sum(data_array)           #return the sum of the squared result to find the signal energy metric
+        data_array = np.array(kwargs['data'])             # convert the data into numpy array
+                                                          #take absolute value and square each element
+        data_array = np.abs(data_array)
+        data_array = np.square(data_array)
+        return np.sum(data_array)                         #return the sum of the squared result to find the signal energy metric
 
 
 
