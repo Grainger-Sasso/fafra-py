9cbc5a47680adf86ae569f1cbfee2ca664313a54
arajni3 <48866532+arajni3@users.noreply.github.com>
2021-07-26 18:57:47 -0500

    Wrote constructor and get and set methods for LightGBM risk classifier

--- src/risk_classification/risk_classifiers/lightgbm_risk_classifier/lightgbm_risk_classifier_code.py
+++ src/risk_classification/risk_classifiers/lightgbm_risk_classifier/lightgbm_risk_classifier_code.py
@@ -1,5 +1,29 @@
-class LightGBM:
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import ShuffleSplit
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+import lightgbm
+
+class LightGBMRiskClassifier:
+    def __init__(self):
+        self.model = lightgbm.LGBMClassifier()
+        self.scaler = preprocessing.StandardScaler()
+
     def get_model(self):
         return self.model
-    def set_model(self, model):
+
+    def set_model(self, model = lightgbm.LGBMClassifier()):
         self.model = model
+
+    def get_scaler(self):
+        return self.scaler
+
+    def set_scaler(self, scaler = preprocessing.StandardScaler()):
+        self.scaler = scaler
+    
