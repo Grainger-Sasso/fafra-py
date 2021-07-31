7982a7e816fc65ff78da2d03b76e5046a69d7e1d
arajni3 <48866532+arajni3@users.noreply.github.com>
2021-07-31 15:07:02 -0500

    Adding repeated initializations for EM algorithm of HMM

--- src/risk_classification/risk_classifiers/hidden_markov_model_risk_classifier/hidden_markov_model_risk_classifier.py
+++ src/risk_classification/risk_classifiers/hidden_markov_model_risk_classifier/hidden_markov_model_risk_classifier.py
@@ -18,9 +18,16 @@ from hmmlearn import hmm
 # seems to be required (since FFT and DFT are both linear combinations of the signals)
 # and hence our Bayes filter needs to cover just Gaussian distributions (hence we'll use a Kalman filter as
 # our Bayes filter since Kalman filter is the Bayes filter for Gaussian distributions)
+
+
 class GaussianHMMRiskClassifier:
+    hmm_params = {}
     def __init__(self, **kwargs):
+        # use covariance_type="full"
+        if kwargs is None:
+            kwargs = {}
         self.model = hmm.GaussianHMM(kwargs)
+        hmm_params = kwargs
 
     def get_model(self) -> hmm.GaussianHMM():
         return self.model
@@ -32,10 +39,14 @@ class GaussianHMMRiskClassifier:
     #     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=random_state)
     #     return x_train, x_test, y_train, y_test
 
-    # probably need to implement k-fold cross validation in fitting model
-    # concatenate y this way to do supervised learning in hmmlearn?
-    def fit_model(self, x: np.ndarray, y: np.ndarray, lengths: int):
-        self.model.fit(np.concatenate(x, y.T, axis=1), lengths)
+    # HMM is used in an unsupervised manner in Yuwono's paper
+    #def fit_model(self, x: np.ndarray, lengths: int):
+        #for n in range(1, 500):
+            #temp_hmmgmm = GaussianHMMRiskClassifier(self.get_model().__dict__)
+            #temp_hmmgmm.fit(x, lengths)
+
+            # if temp_hmmgmm's model is better than self.model:
+                # self.set_model(temp_
 
 # insert params into hmm_gmm_risk_classifier below (using names of hmm.GaussianHMM params; no need to input in a dict)
 # gaussian_hmm_risk_classifier = GaussianHMMRiskClassifier(*insert params w/names here*)
