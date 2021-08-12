9457175d4576fcf0f1d7f7c10169edcf61b7a1c4
Grainger-Sasso <gsasso16@gmail.com>
2021-08-12 17:26:32 -0400

    Fixed some indexing

--- src/risk_classification/input_metrics/metric_instances/zero_crossing_metric.py
+++ src/risk_classification/input_metrics/metric_instances/zero_crossing_metric.py
@@ -11,36 +11,32 @@ class Metric(RiskClassificationInputMetric):
         super().__init__(METRIC_NAME)
 
     def generate_metric(self, **kwargs):
-        return self._find_zero_crossing(kwargs['data'],kwargs['enable_mean_crossing'])
+        return self._find_zero_crossing(kwargs['data'], kwargs['enable_mean_crossing'])
     
-    def _find_zero_crossing(self, data,enable_mean_crossing = 0):
+    def _find_zero_crossing(self, data, enable_mean_crossing=True):
         """
         zero crossing
         :param data:input would be a 1D array that indicates the data array
-        :param sampling_frequency:would be a single float number
         :return:the zero crossing or mean crossing for an array
         """
         data_array = np.array(data)
         real_len = len(data_array)-1
-        if real_len < 1:                #perform error checking if input data too short
-            raise TooFewDataError
+        # perform error checking if input data too short
+        if real_len < 1:
+            raise ValueError('Too few data for zero-crossing metric')
         sampling_frequency=1/len(data_array)
-        
-        if enable_mean_crossing:        #switch to mean crossing calculation
+
+        # switch to mean crossing calculation
+        if enable_mean_crossing:
             data_array = data_array-np.mean(data_array)
-        result = data_array[1:len(data_array)]*data_array[0:real_len]           #calculte if there is zero crossing by multiply nearby elements
-        for idx, elem in enumerate(result):                 #count zero crossing frequency
+        # calculte if there is zero crossing by multiply nearby elements
+        result = data_array[1:len(data_array)]*data_array[0:real_len]
+        # count zero crossing frequency
+        for idx, elem in enumerate(result):
             if elem >= 0:
                 result[idx] = 0 
             else:
                 result[idx] = 1
-        result = result.sum()/((1/sampling_frequency)-1)                        #find final crossing rate by dividing time internal
+        # find final crossing rate by dividing time internal
+        result = result.sum()/((1/sampling_frequency)-1)
         return result
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-class TooFewDataError(Error):
-    """Raised when input data array has too few elements"""
-    pass
-
-        
