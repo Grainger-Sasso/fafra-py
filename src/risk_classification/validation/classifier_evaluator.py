28625621cf523dbe6542d738c198470da17f94ab
Grainger-Sasso <gsasso16@gmail.com>
2021-11-03 16:30:01 -0400

    basics of class

--- src/risk_classification/validation/classifier_evaluator.py
+++ src/risk_classification/validation/classifier_evaluator.py
@@ -1,6 +1,16 @@
+from typing import List
+
+from src.risk_classification.validation.classifier_metrics import ClassifierMetrics
+from src.risk_classification.risk_classifiers.classifier import Classifier
+from src.risk_classification.input_metrics.input_metrics import InputMetrics
 
 
 class ClassifierEvaluator:
     def __init__(self):
+        pass
+
+    def run_models_evaluation(self, eval_metrics: List[ClassifierMetrics],
+                              classifiers: List[Classifier],
+                              input_data: InputMetrics, output_path):
         pass
 
---
+++ src/risk_classification/validation/classifier_metrics.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+
+class ClassifierMetrics(Enum):
+    EXAMPLE = 'example'
+
+    def get_name(self):
+        # Self is the member here
+        return self.name
+
+    def get_value(self):
+        # Self is the member here
+        return self.value
+
+    @classmethod
+    def get_all_enum_entries(cls):
+        return [metric_name for metric_name in cls]
+
+    @classmethod
+    def get_all_names(cls):
+        return [metric_name.name for metric_name in cls]
+
+    @classmethod
+    def get_all_values(cls):
+        return [metric_name.value for metric_name in cls]
