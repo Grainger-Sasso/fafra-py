f5cb558db6c2bb4ad8fd3912066121fa38db09ff
Grainger-Sasso <gsasso16@gmail.com>
2022-07-19 17:07:10 -0400

    TASK-43: adding attribute to reference the user data used to create the input metric

--- src/risk_classification/input_metrics/input_metrics.py
+++ src/risk_classification/input_metrics/input_metrics.py
@@ -4,12 +4,14 @@ from typing import Dict
 
 from src.risk_classification.input_metrics.metric_names import MetricNames
 from src.risk_classification.input_metrics.input_metric import InputMetric
+from src.dataset_tools.risk_assessment_data.user_data import UserData
 
 
 class InputMetrics:
     def __init__(self):
         self.metrics: Dict[MetricNames: InputMetric] = {}
         self.labels: np.array = []
+        self.user_data: UserData = None
 
     def get_metrics(self):
         return self.metrics
@@ -20,11 +22,17 @@ class InputMetrics:
     def get_labels(self):
         return self.labels
 
+    def get_user_data(self) -> UserData:
+        return self.user_data
+
     def set_labels(self, labels: np.array):
         self.labels = labels
 
     def set_metric(self, name: MetricNames, metric: InputMetric):
         self.metrics[name] = metric
+
+    def set_user_data(self, user_data: UserData):
+        self.user_data: UserData = user_data
 
     def get_metric_matrix(self):
         metrics = []
