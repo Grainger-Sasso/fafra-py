776c7c88a0b169eb4376a10f0986b80312a8bc98
Grainger-Sasso <gsasso16@gmail.com>
2022-07-21 15:59:03 -0400

    TASK-43: Getting model trainer working

--- src/fibion_mvp/ltmm_model_trainer.py
+++ src/fibion_mvp/ltmm_model_trainer.py
@@ -3,6 +3,8 @@ import numpy as np
 import glob
 import wfdb
 import joblib
+import time
+from typing import List
 
 from src.dataset_tools.dataset_builders.builder_instances.ltmm_dataset_builder import DatasetBuilder
 from src.risk_classification.input_metrics.metric_generator import MetricGenerator
@@ -23,13 +25,15 @@ from src.dataset_tools.risk_assessment_data.clinical_demographic_data import Cli
 
 
 class ModelTrainer:
-    def __init__(self, dataset_path, clinical_demo_path, segment_dataset, epoch_size , custom_metric_names):
+    def __init__(self, dataset_path, clinical_demo_path, segment_dataset,
+                 epoch_size, custom_metric_names, gait_metric_names: List[str]):
         self.dataset_path = dataset_path
         self.clinical_demo_path = clinical_demo_path
         self.segment_dataset = segment_dataset
         self.epoch_size = epoch_size
         # self.dataset = self.build_dataset()
         self.custom_metric_names = custom_metric_names
+        self.gait_metric_names: List[str] = gait_metric_names
         self.rc = LightGBMRiskClassifier({})
         self.sampling_frequency = 100.0
         self.units = {'vertical-acc': 'm/s^2', 'mediolateral-acc': 'm/s^2',
@@ -38,7 +42,12 @@ class ModelTrainer:
         self.height = 1.75
 
     def generate_model(self, skdh_output_path, model_output_path, file_name):
-        self.generate_input_metrics(skdh_output_path)
+        time0 = time.time()
+        input_metrics = self.generate_input_metrics(skdh_output_path)
+        print(time.time() - time0)
+        print(input_metrics.get_metrics())
+        print('HUH?')
+        # Scale input metrics
         # Preprocess data
         # self.preprocess_data()
         # # Generate custom metrics
@@ -56,15 +65,19 @@ class ModelTrainer:
 
     def generate_input_metrics(self, skdh_output_path):
         head_df_paths = self._generate_header_and_data_file_paths()
-        input_metrics = InputMetrics()
+        input_metrics = self.initialize_input_metrics()
         for name, header_and_data_file_path in head_df_paths.items():
             # Load the data and compute the input metrics for the file
             ds = self.create_dataset(header_and_data_file_path)
             self.preprocess_data(ds)
             custom_input_metrics: InputMetrics = self.generate_custom_metrics(ds)
             skdh_input_metrics = self.generate_skdh_metrics(ds, skdh_output_path)
-            print('si papi')
-            pass
+            input_metrics = self.format_input_metrics(input_metrics,
+                                                      custom_input_metrics, skdh_input_metrics)
+            print('yerp')
+        input_metrics = self.finalize_metric_formatting(input_metrics)
+        return self.rc.scale_input_data(input_metrics)
+
 
     def create_dataset(self, header_and_data_file_path):
         dataset = []
@@ -186,6 +199,36 @@ class ModelTrainer:
                        ml_acc_data, ap_acc_data, yaw_gyr_data, pitch_gyr_data,
                        roll_gyr_data, time)
 
+    def initialize_input_metrics(self):
+        input_metrics = InputMetrics()
+        for name in self.custom_metric_names:
+            input_metrics.set_metric(name, [])
+        for name in self.gait_metric_names:
+            input_metrics.set_metric(name, [])
+        input_metrics.set_labels([])
+        return input_metrics
+
+    def finalize_metric_formatting(self, input_metrics: InputMetrics):
+        new_ims = InputMetrics
+        for name, metric in input_metrics.get_metrics().items():
+            im = InputMetric(name, np.array(metric))
+            new_ims.set_metric(name, im)
+        new_ims.set_labels(np.array(input_metrics.get_labels()))
+        return new_ims
+
+    def format_input_metrics(self, input_metrics,
+                             custom_input_metrics: InputMetrics,
+                             skdh_input_metrics):
+        for user_metrics in skdh_input_metrics:
+            gait_metrics = user_metrics['gait_metrics']
+            for name, val in gait_metrics.items():
+                if name in self.gait_metric_names:
+                    input_metrics.get_metric(name).append(val)
+        for name, metric in custom_input_metrics.get_metrics().items():
+            input_metrics.get_metric(name).extend(metric.get_value().tolist())
+        input_metrics.get_labels().extend(custom_input_metrics.get_labels())
+        return input_metrics
+
     # def build_dataset(self):
     #     db = DatasetBuilder()
     #     return db.build_dataset(
@@ -216,8 +259,8 @@ class ModelTrainer:
     #         im = InputMetric(name, np.array(val))
     #         custom_input_metrics.set_metric(name, im)
     #     print(model_gait_metrics)
-    #
-    #     # Scale the input metrics
+
+        # Scale the input metrics
     #
     # def train_model(self, input_metrics, input_metric_names):
     #     x, names = input_metrics.get_metric_matrix()
@@ -255,7 +298,7 @@ def main():
     #         MetricNames.SIGNAL_MAGNITUDE_AREA
     #     ]
     # )
-    metric_names = tuple(
+    custom_metric_names = tuple(
         [
             MetricNames.SIGNAL_MAGNITUDE_AREA,
             MetricNames.COEFFICIENT_OF_VARIANCE,
@@ -267,7 +310,15 @@ def main():
             MetricNames.FAST_FOURIER_TRANSFORM
         ]
     )
-    mt = ModelTrainer(dp, cdp, seg, epoch, metric_names)
+    gait_metric_names = [
+            'PARAM:gait speed: mean',
+            'PARAM:gait speed: std',
+            'BOUTPARAM:gait symmetry index: mean',
+            'BOUTPARAM:gait symmetry index: std',
+            'PARAM:cadence: mean',
+            'PARAM:cadence: std'
+        ]
+    mt = ModelTrainer(dp, cdp, seg, epoch, custom_metric_names, gait_metric_names)
     mt.generate_model('','','')
     print('yup')
 
