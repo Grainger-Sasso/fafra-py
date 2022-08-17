a2b30f4286f07711f9c77352e4e4d4e0c1111b14
Grainger-Sasso <gsasso16@gmail.com>
2022-08-17 17:20:28 -0400

    TASK-48: refactoring the report generation

--- src/fibion_mvp/report_generation/report_generator.py
+++ src/fibion_mvp/report_generation/report_generator.py
@@ -15,6 +15,112 @@ rcParams['axes.spines.right'] = False
 HEIGHT = 297
 WIDTH = 210
 
+class ReportGenerator:
+    def __init__(self):
+        pass
+
+    def generate_report(self):
+        pdf = PDF()
+
+        image_list = []
+        # im=Image.open('./Daily Activity Summary.png')
+        image_list.append(
+            '/home/grainger/PycharmProjects/fafra-py/src/fibion_mvp/report_generation/Daily Activity Summary.png')
+        image_list.append('/home/grainger/PycharmProjects/fafra-py/src/fibion_mvp/report_generation/pie_graph.png')
+        image_list.append('/home/grainger/PycharmProjects/fafra-py/src/fibion_mvp/report_generation/pie_graph.png')
+        # for elem in image_list:
+        #     pdf.print_page(elem)
+        pdf.add_page()
+        # pdf.set_fill_color(223,218,218)
+        # pdf.rect(10,40,190,60,style='F')
+        pdf.set_fill_color(211, 211, 211)
+        pdf.rect(x=20, y=255 - 2 * HEIGHT / 4, w=WIDTH - 40, h=60, style='DF')
+        pdf.rect(x=20, y=245 - HEIGHT / 4, w=WIDTH - 40, h=60, style='DF')
+
+        pdf.print_page(image_list)
+        pdf.rect(x=40, y=265 - 2 * HEIGHT / 4, w=45, h=45, round_corners=True)
+        pdf.rect(x=40, y=256 - HEIGHT / 4, w=45, h=45, round_corners=True)
+
+        pdf.set_font("helvetica", "", 16)
+
+        # Key Layout Variables
+        margin_x = 20
+        margin_y = 5
+        rect_width = 170
+        rect_height = 60
+        current_x = 0
+        current_y = 0
+
+        # Variables from JSON
+        fall_risk_score = 'medium'
+        gait_speed = '10m/s'
+        cadence = '0/100'
+        steps_per_day = '120000'
+        user_name = 'such a long name'
+        report_generated = 'long ass report'
+        user_id = '123456789'
+        useless_field = 'place holder'
+        collection_period = '2020/12/12-2022/08/09'
+
+        # Header
+        current_y += margin_y
+        pdf.set_fill_color(211, 211, 211)
+        # Header rectangles and logo image
+        pdf.rect(margin_x, current_y, rect_width, 30, style='DF')
+        pdf.image("carapace_logo.jpg", margin_x + 6, current_y + 1, 28, 28)
+        pdf.rect(margin_x + 45, current_y + 2, 120, 26, style='')
+        # Required fields for header
+        pdf.set_font("helvetica", "", 11)
+        pdf.text(margin_x + 50, current_y + 7, "User Name: " + user_name)
+        pdf.text(margin_x + 105, current_y + 7, "Report Generated: " + report_generated)
+        pdf.text(margin_x + 50, current_y + 15, "User ID: " + user_id)
+        pdf.text(margin_x + 105, current_y + 15, "Useless field: " + useless_field)
+        pdf.text(margin_x + 50, current_y + 23, "Data Collection Period: " + collection_period)
+        pdf.set_font("helvetica", "", 16)
+        current_y += 30
+
+        # Fall Risk Report
+        # Report Header
+        current_y += margin_y
+        pdf.rect(margin_x, current_y, rect_width, rect_height, style='DF')
+        pdf.text(margin_x + 75, current_y + 10, "Fall Risk Report")
+        pdf.set_font("helvetica", "", 13)
+        pdf.rect(margin_x + 5, current_y + 15, w=50, h=40, round_corners=True)
+        pdf.text(margin_x + 15, current_y + 20, "Fall Risk Score")
+        # Set color for fall-risk rectangles
+        if fall_risk_score == 'high':
+            pdf.set_fill_color(255, 0, 0)
+        if fall_risk_score == 'medium':
+            pdf.set_fill_color(255, 255, 0)
+        if fall_risk_score == 'low':
+            pdf.set_fill_color(0, 255, 0)
+        high_style = 'DF' if fall_risk_score == 'high' else 'D'
+        medium_style = 'DF' if fall_risk_score == 'medium' else 'D'
+        low_style = 'DF' if fall_risk_score == 'low' else 'D'
+        pdf.rect(margin_x + 15, current_y + 22, w=30, h=8, style=high_style)
+        pdf.text(margin_x + 24, current_y + 27, "HIGH")
+        pdf.rect(margin_x + 15, current_y + 32, w=30, h=8, style=medium_style)
+        pdf.text(margin_x + 21, current_y + 37, "MEDIUM")
+        pdf.rect(margin_x + 15, current_y + 42, w=30, h=8, style=low_style)
+        pdf.text(margin_x + 25, current_y + 47, "LOW")
+        pdf.set_fill_color(211, 211, 211)
+        # Fall risk indicators and its fields
+        pdf.rect(margin_x + 80, current_y + 15, w=80, h=40, round_corners=True)
+        pdf.text(margin_x + 100, current_y + 20, "Fall Risk Indicators")
+        pdf.set_font("helvetica", "", 11)
+        pdf.text(margin_x + 85, current_y + 30, "Gait Speed")
+        pdf.text(margin_x + 107, current_y + 30, "|")
+        pdf.text(margin_x + 90, current_y + 40, gait_speed)
+        pdf.text(margin_x + 110, current_y + 30, "Cadence")
+        pdf.text(margin_x + 127, current_y + 30, "|")
+        pdf.text(margin_x + 115, current_y + 40, cadence)
+        pdf.text(margin_x + 130, current_y + 30, "Steps per day")
+        pdf.text(margin_x + 133, current_y + 40, steps_per_day)
+        pdf.set_font("helvetica", "", 16)
+        current_y += rect_height
+
+        pdf.output('/home/grainger/Desktop/skdh_testing/fafra_results/reports/whole_report/SalesReport.pdf')
+
 
 class PDF(FPDF):
     def __init__(self):
@@ -111,102 +217,10 @@ class PDF(FPDF):
         self.lines()
 
 
-pdf = PDF()
+def main():
+    rg = ReportGenerator()
+    rg.generate_report()
 
-image_list = []
-# im=Image.open('./Daily Activity Summary.png')
-image_list.append('./Daily Activity Summary.png')
-image_list.append('./pie_graph.png')
-image_list.append('./pie_graph.png')
-# for elem in image_list:
-#     pdf.print_page(elem)
-pdf.add_page()
-# pdf.set_fill_color(223,218,218)
-# pdf.rect(10,40,190,60,style='F')
-pdf.set_fill_color(211, 211, 211)
-pdf.rect(x=20, y=255 - 2 * HEIGHT / 4, w=WIDTH - 40, h=60, style='DF')
-pdf.rect(x=20, y=245 - HEIGHT / 4, w=WIDTH - 40, h=60, style='DF')
 
-pdf.print_page(image_list)
-pdf.rect(x=40, y=265 - 2 * HEIGHT / 4, w=45, h=45, round_corners=True)
-pdf.rect(x=40, y=256 - HEIGHT / 4, w=45, h=45, round_corners=True)
-
-pdf.set_font("helvetica", "", 16)
-
-# Key Layout Variables
-margin_x = 20
-margin_y = 5
-rect_width = 170
-rect_height = 60
-current_x = 0
-current_y = 0
-
-# Variables from JSON
-fall_risk_score = 'medium'
-gait_speed = '10m/s'
-cadence = '0/100'
-steps_per_day = '120000'
-user_name = 'such a long name'
-report_generated = 'long ass report'
-user_id = '123456789'
-useless_field = 'place holder'
-collection_period = '2020/12/12-2022/08/09'
-
-# Header
-current_y += margin_y
-pdf.set_fill_color(211, 211, 211)
-# Header rectangles and logo image
-pdf.rect(margin_x, current_y, rect_width, 30, style='DF')
-pdf.image("carapace_logo.jpg", margin_x + 6, current_y + 1, 28, 28)
-pdf.rect(margin_x + 45, current_y + 2, 120, 26, style='')
-# Required fields for header
-pdf.set_font("helvetica", "", 11)
-pdf.text(margin_x + 50, current_y + 7, "User Name: " + user_name)
-pdf.text(margin_x + 105, current_y + 7, "Report Generated: " + report_generated)
-pdf.text(margin_x + 50, current_y + 15, "User ID: " + user_id)
-pdf.text(margin_x + 105, current_y + 15, "Useless field: " + useless_field)
-pdf.text(margin_x + 50, current_y + 23, "Data Collection Period: " + collection_period)
-pdf.set_font("helvetica", "", 16)
-current_y += 30
-
-# Fall Risk Report
-# Report Header
-current_y += margin_y
-pdf.rect(margin_x, current_y, rect_width, rect_height, style='DF')
-pdf.text(margin_x + 75, current_y + 10, "Fall Risk Report")
-pdf.set_font("helvetica", "", 13)
-pdf.rect(margin_x + 5, current_y + 15, w=50, h=40, round_corners=True)
-pdf.text(margin_x + 15, current_y + 20, "Fall Risk Score")
-# Set color for fall-risk rectangles
-if fall_risk_score == 'high':
-    pdf.set_fill_color(255, 0, 0)
-if fall_risk_score == 'medium':
-    pdf.set_fill_color(255, 255, 0)
-if fall_risk_score == 'low':
-    pdf.set_fill_color(0, 255, 0)
-high_style = 'DF' if fall_risk_score == 'high' else 'D'
-medium_style = 'DF' if fall_risk_score == 'medium' else 'D'
-low_style = 'DF' if fall_risk_score == 'low' else 'D'
-pdf.rect(margin_x + 15, current_y + 22, w=30, h=8, style=high_style)
-pdf.text(margin_x + 24, current_y + 27, "HIGH")
-pdf.rect(margin_x + 15, current_y + 32, w=30, h=8, style=medium_style)
-pdf.text(margin_x + 21, current_y + 37, "MEDIUM")
-pdf.rect(margin_x + 15, current_y + 42, w=30, h=8, style=low_style)
-pdf.text(margin_x + 25, current_y + 47, "LOW")
-pdf.set_fill_color(211, 211, 211)
-# Fall risk indicators and its fields
-pdf.rect(margin_x + 80, current_y + 15, w=80, h=40, round_corners=True)
-pdf.text(margin_x + 100, current_y + 20, "Fall Risk Indicators")
-pdf.set_font("helvetica", "", 11)
-pdf.text(margin_x + 85, current_y + 30, "Gait Speed")
-pdf.text(margin_x + 107, current_y + 30, "|")
-pdf.text(margin_x + 90, current_y + 40, gait_speed)
-pdf.text(margin_x + 110, current_y + 30, "Cadence")
-pdf.text(margin_x + 127, current_y + 30, "|")
-pdf.text(margin_x + 115, current_y + 40, cadence)
-pdf.text(margin_x + 130, current_y + 30, "Steps per day")
-pdf.text(margin_x + 133, current_y + 40, steps_per_day)
-pdf.set_font("helvetica", "", 16)
-current_y += rect_height
-
-pdf.output('SalesReport.pdf')
+if __name__ == '__main__':
+    main()
