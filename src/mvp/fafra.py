1aeb30e9c7b5683eeb1e3dfe9a045cb1876322b2
Grainger-Sasso <gsasso16@gmail.com>
2022-08-25 17:16:22 -0400

    TASK-49: building out metric generation

--- src/mvp/fafra.py
+++ src/mvp/fafra.py
@@ -1,8 +1,12 @@
+import os
+import glob
+import json
 from typing import List
 
 
 from src.risk_classification.input_metrics.metric_names import MetricNames
 from src.mvp.report_generation.report_generator import ReportGenerator
+from src.mvp.mbientlab_dataset_builder import MbientlabDatasetBuilder
 
 
 class FaFRA:
@@ -62,14 +66,48 @@ class FaFRA:
 
     def perform_risk_assessment(self, assessment_path):
         # Generate risk metrics
-        mg = MetricGen()
-        ra_metrics = mg.generate_ra_metrics(assessment_path)
+        ra_metrics = MetricGen().generate_ra_metrics(assessment_path, self.custom_metric_names, self.gait_metric_names)
         # Assess risk using risk model
-        model = Model()
-        ra_results = model.assess_fall_risk(self.ra_model_path)
+        ra_results = Model().assess_fall_risk(self.ra_model_path)
         # Generate risk report
         rg = ReportGenerator()
         rg.generate_report(assessment_path, '', '', '')
+
+
+class MetricGen:
+    def __init__(self):
+        pass
+
+    def generate_ra_metrics(self, assessment_path, custom_metric_names, gait_metric_names):
+        data = DataLoader().load_data(assessment_path)
+
+
+class DataLoader:
+    def load_json_data(self, file_path):
+        print(file_path)
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+        return data
+
+    def load_data(self, assessment_path):
+        # Load in the assessment data file (contains device type of the IMU data)
+        assess_info_path = os.path.join(assessment_path, 'assessment_info.json')
+        assess_info = self.load_json_data(assess_info_path)
+        data_type = assess_info['device_type']
+        # Load in the user data json file (contains demographic data of the user)
+        collect_data_path = os.path.join(assessment_path, 'collected_data')
+        user_info_path = os.path.join(collect_data_path, 'user_info.json')
+        user_info = self.load_json_data(user_info_path)
+        # Load in the IMU data file based on type
+        imu_data_filename = [filename for filename in os.listdir(collect_data_path) if filename.startswith("imu_data")][0]
+        imu_data_path = os.path.join(collect_data_path, imu_data_filename)
+        # Build dataset objects
+        pass
+
+    def read_imu_data(self, data_type, imu_data_path, user_info):
+        if data_type.lower() == 'mbientlab':
+            user_data:  = MbientlabDatasetBuilder().build_single_user(imu_data_path, user_info)
+        pass
 
 
 class Model:
@@ -80,15 +118,11 @@ class Model:
         pass
 
 
-class MetricGen:
-    def __init__(self):
-        pass
-
-    def generate_ra_metrics(self, assessment_path):
-        pass
-
-
 def main():
     fafra = FaFRA()
-    path = ''
-    ra = fafra.perform_risk_assessment('/home/grainger/Desktop/test_risk_assessments/customers/customer_Grainger/site_Breed_Road/batch_0000000000000001_2022_08_25/assessment_0000000000000001_2022_08_25/')
+    path = '/home/grainger/Desktop/test_risk_assessments/customers/customer_Grainger/site_Breed_Road/batch_0000000000000001_2022_08_25/assessment_0000000000000001_2022_08_25/'
+    ra = fafra.perform_risk_assessment(path)
+
+
+if __name__ == '__main__':
+    main()
